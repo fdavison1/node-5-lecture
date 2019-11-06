044b3f90d3f4784c165fed07415facbff6b01f68
Fred Davison <fredbook@Freds-MacBook-Pro.local>
2019-11-06 13:43:46 -0700

    front end added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,91 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      emailInput: '',
+      passwordInput: '',
+      user: {}
+    }
+  }
+
+  async signup() {
+    let res = await axios.post('/auth/register', {
+      email: this.state.emailInput,
+      password: this.state.passwordInput
+    })
+    this.setState({
+      user: res.data.userData
+    })
+  }
+
+  async login() {
+    let res = await axios.post('/auth/login', {
+      email: this.state.emailInput,
+      password: this.state.passwordInput
+    })
+    this.setState({
+      user: res.data.userData
+    })
+  }
+
+  async logout() {
+    await axios.get('/auth/logout')
+    this.setState({ user: {} })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="header">
+          <h1 className="header__title">
+            Authorize <span className="header__title--emphasize">Me</span>
+          </h1>
+        </header>
+        <div className="modal">
+          <p className="modal__field">
+            Email:
+            <input
+              onChange={e => this.setState({ emailInput: e.target.value })}
+              type="text"
+            />
+          </p>
+          <p className="modal__field">
+            Password:
+            <input
+              onChange={e => this.setState({ passwordInput: e.target.value })}
+              type="password"
+            />
+          </p>
+          <div className="modal__button-container">
+            <button
+              className="button--theme-green"
+              onClick={() => this.signup()}
+            >
+              Signup
+            </button>
+            <button
+              className="button--theme-green"
+              onClick={() => this.login()}
+            >
+              Login
+            </button>
+            <button
+              className="button--theme-green"
+              onClick={() => this.logout()}
+            >
+              Logout
+            </button>
+          </div>
+        </div>
+        <hr />
+        <p className="user-info">USER: {JSON.stringify(this.state.user)}</p>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
